2ee1faaa1a07cf268df84c84d30ac3aab6ef376d
Vynz <76029153+VincelyVynz@users.noreply.github.com>
2025-10-25 11:57:43 +0530

    Write models.py

---
+++ models.py
@@ -0,0 +1,10 @@
+from sqlalchemy import Column, Integer, String, Float
+from database import Base
+
+class Student(Base):
+    __tablename__ = "students"
+
+    id = Column(Integer, primary_key= True, index= True)
+    name = Column(String, nullable= False)
+    subject =Column(String, nullable = False)
+    marks = Column(Float, nullable= False)
